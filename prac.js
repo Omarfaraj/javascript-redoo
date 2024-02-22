157a140e55f16fce9d4557c8251d232b1ec02a2c
blvck <omarfaraj355@gmail.com>
2024-02-22 21:19:27 +0300

    javascript recap

---
+++ prac.js
@@ -0,0 +1,47 @@
+let age = 22;
+let year = 2001;
+
+// log things to console
+console.log(age, year);
+
+age = 25;
+console.log(age);
+
+const points = 84;
+
+console.log(points);
+
+var marks = 82;
+console.log(marks);
+
+//strings
+console.log('testing 1, 2. Strings check');
+
+let email = 'omarfaraj355@gmail.com';
+
+// string concatenation
+let firstName = 'Faraj';
+let lastName = 'Omar';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+//gettingn character
+console.log(fullName[0]);
+
+//string length
+console.log(fullName.length);
+
+//string methods
+console.log(fullName.toUpperCase());
+let results = fullName.toLowerCase();
+console.log(results);
+
+let index = email.indexOf('@')
+console.log(index);
+
+//other methods
+let mail = 'omarfaraj355@gmail.com';
+let output = mail.lastIndexOf('m');
+
+console.log(output);
